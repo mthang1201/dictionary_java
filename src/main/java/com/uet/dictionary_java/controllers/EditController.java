619140b5f9a704ba75a9bd704cdc58d218d69224
mthang1201 <mthang1201@gmail.com>
2024-08-03 10:47:28 +0700

    add pagination

--- src/main/java/com/uet/dictionary_java/controllers/EditController.java
+++ src/main/java/com/uet/dictionary_java/controllers/EditController.java
@@ -2,12 +2,14 @@ package com.uet.dictionary_java.controllers;
 
 import com.uet.dictionary_java.WordEntity;
 import com.uet.dictionary_java.WordService;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Region;
 
 import java.util.List;
 
@@ -19,7 +21,7 @@ public class EditController {
     public ScrollPane scrollPane;
 
     @FXML
-    public Button pageBtn;
+    public TextField pageTextField;
 
     private int currentPage = 1;
 
@@ -28,14 +30,19 @@ public class EditController {
         gridPane.add(label, rowIndex, columnIndex);
     }
 
-    @FXML
-    private void initialize() {
+    private void setCurrentPage() {
+        currentPage = Integer.parseInt(pageTextField.getText());
+        reloadPage();
+    }
+
+    private void reloadPage() {
+        gridPane.getChildren().clear();
         WordService wordService = new WordService();
         List<WordEntity> wordEntities = wordService.findAllByPage(currentPage, 20);
-        pageBtn.setText(String.valueOf(currentPage));
+        pageTextField.setText(String.valueOf(currentPage));
         int columnIndex = 0;
         for (WordEntity wordEntity : wordEntities) {
-            addLabelToGridPane(String.valueOf(wordEntity.getId()), 0, columnIndex);
+            //addLabelToGridPane(String.valueOf(wordEntity.getId()), 0, columnIndex);
             addLabelToGridPane(wordEntity.getName(), 1, columnIndex);
             addLabelToGridPane(wordEntity.getIpa(), 2, columnIndex);
             addLabelToGridPane(wordEntity.getDefinition(), 3, columnIndex);
@@ -45,17 +52,37 @@ public class EditController {
     }
 
     @FXML
+    private void initialize() {
+        pageTextField.setOnKeyPressed(event -> {
+            if (event.getCode() == KeyCode.ENTER) {
+                setCurrentPage();
+            }
+        });
+
+        pageTextField.focusedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean wasFocused, Boolean isNowFocused) {
+                if (!isNowFocused) {
+                    setCurrentPage();
+                }
+            }
+        });
+
+        reloadPage();
+    }
+
+    @FXML
     private void decreasePage() {
         if (currentPage == 1) return;
         currentPage--;
-        initialize();
+        reloadPage();
     }
 
     @FXML
     private void increasePage() {
         if (currentPage == 1000) return;
         currentPage++;
-        initialize();
+        reloadPage();
     }
 
 }
